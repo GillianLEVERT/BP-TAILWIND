dd3ade373dce3094539aac075e6593b324706424
GillianLEVERT <gillian.levert@gmail.com>
2023-02-16 14:11:01 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom'
+import { NotFound } from './components/NotFound/NotFound'
+import { Home } from './components/Home/Home'
+
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
